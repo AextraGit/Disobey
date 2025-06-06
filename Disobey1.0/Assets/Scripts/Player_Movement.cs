1a7b980d25f1b920a8d0cb5d5ba4d7e2366f28a1
Jaro <jarobendominik@gmail.com>
2025-06-06 18:32:26 +0200

    documentation fix

--- Disobey1.0/Assets/Scripts/Player_Movement.cs
+++ Disobey1.0/Assets/Scripts/Player_Movement.cs
@@ -22,8 +22,8 @@ public class Player_Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 viewDirectionVector = new Vector2(transform.forward.x, transform.forward.z); // direction in which the Player is looking (used as basis vector b1)
-        Vector2 viewDirectionRightVector = new Vector2(transform.right.x, transform.right.z); // basis vector b2
+        Vector2 viewDirectionVector = new Vector2(transform.forward.x, transform.forward.z); // direction in which the Player is looking (used as basis vector b2)
+        Vector2 viewDirectionRightVector = new Vector2(transform.right.x, transform.right.z); // basis vector b1
         Vector2 moveDirection = (inputDirectionVector.x * viewDirectionRightVector + inputDirectionVector.y * viewDirectionVector).normalized; // inputDirectionVector but viewDirection used as basis (x * b1 + y * b2)
         rb.linearVelocity = new Vector3(moveDirection.x, 0, moveDirection.y) * moveSpeed;
     }
