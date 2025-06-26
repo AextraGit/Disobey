fbea23ac7f5b5aa025a04aa516d0b50e4942fabc
Jaro <jarobendominik@gmail.com>
2025-06-26 18:57:15 +0200

    added police movement to follow the player using nav mesh

--- Disobey1.0/Assets/Scripts/Enemies/PoliceMovement.cs
+++ Disobey1.0/Assets/Scripts/Enemies/PoliceMovement.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PoliceMovement : MonoBehaviour
 {
     private Rigidbody rb;
+    public Transform player;
+    private NavMeshAgent agent;
+    public float followDistance = 10f;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
         rb.constraints = RigidbodyConstraints.FreezeRotation; // rotation turned off
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if (distance <= followDistance)
+        {
+            agent.SetDestination(player.position);
+        }
+
     }
 }
