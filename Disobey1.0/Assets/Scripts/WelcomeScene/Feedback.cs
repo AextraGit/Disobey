93d3ff6468ae95c32594801c386a95e83ae4b69a
uexyi <91570411+uexyi@users.noreply.github.com>
2025-07-06 13:42:16 +0200

    Changed the display time of the messages, added some better interchangeable strings for the tutorial text for firing the weapon, also changed the degree of the throw and the force

--- Disobey1.0/Assets/Scripts/WelcomeScene/Feedback.cs
+++ Disobey1.0/Assets/Scripts/WelcomeScene/Feedback.cs
@@ -7,22 +7,26 @@ public class Feedback : MonoBehaviour
 {
     public GameObject welcomeText;
     public TextMeshProUGUI countText;
-    public float displayTime = 5f;
+    public float displayTimeWelcome = 5f;
+    public float displayTimeMessages = 3f;
 
     public InputActionReference doContinue;
 
     private string[] tutorialSteps;
     private int currentStep;
+    private string welcomeMessage = "Welcome to Disobey!";
+    private string continueButtonText = "Space";
+    private string throwBrickButtonText = "left mouse button";
 
     void Start()
     {
         currentStep = 0;
         tutorialSteps = new string[]
         {
-            "Welcome to Disobey!",
-            "Use W, A, S, D to move to the front, left, back and right. Press Space to continue.",
+            welcomeMessage,
+            "Use W, A, S, D to move to the front, left, back and right. Press " + continueButtonText + " to continue.",
             "Use the mouse to look around. Press Space to continue.",
-            "Press Q to throw a brick. Try out the controls and hit the boxes. Press Space to continue.",
+            "Press the " + throwBrickButtonText + " to throw a brick. Try out the controls and hit the boxes or the police men. Press Space to continue.",
             "Throw bricks at the targets to damage them. Press Space to continue.",
             "Now you know how to play the game, have fun! Press Space to end the tutorial."
         };
@@ -52,7 +56,7 @@ public class Feedback : MonoBehaviour
     IEnumerator ShowFirstStep()
     {
         welcomeText.GetComponent<TextMeshProUGUI>().text = tutorialSteps[currentStep];
-        yield return new WaitForSeconds(displayTime);
+        yield return new WaitForSeconds(displayTimeWelcome);
         currentStep++; // Move to the next step after displaying the first one
         ShowTutorialStep(); // Show the next step immediately
     }
@@ -75,7 +79,7 @@ public class Feedback : MonoBehaviour
     {
         // Disable input while waiting for the display time
         doContinue.action.Disable();
-        yield return new WaitForSeconds(displayTime);
+        yield return new WaitForSeconds(displayTimeMessages);
         // Re-enable input after the display time
         doContinue.action.Enable();
     }
