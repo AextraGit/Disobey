4318023f80f36899f621668c779763b8fd162624
uexyi <91570411+uexyi@users.noreply.github.com>
2025-07-06 13:13:58 +0200

    Added a health script for the policemen, will make the police men disappear after their health dropped to 0

---
+++ Disobey1.0/Assets/Scripts/NPC/Police/PoliceHealth.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PoliceHealth : MonoBehaviour
+{
+    public float health = 50f; // Initial health of the police
+
+    private void Start()
+    {
+        Debug.Log("Police Man initialized with health: " + health);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Brick"))
+        {
+            float damage = collision.gameObject.GetComponent<Brick>().damage;
+            Debug.Log($"Collision detected with Cube! Damage: {damage}");
+            TakeDamage(damage);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        Debug.Log($"Police Health: {health}");
+
+        if (health <= 0)
+        {
+            DestroyBox();
+        }
+    }
+
+    private void DestroyBox()
+    {
+        Debug.Log("Police destroyed!");
+        Destroy(gameObject);
+    }
+}
