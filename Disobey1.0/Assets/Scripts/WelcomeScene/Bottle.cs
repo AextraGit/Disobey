293d163d28262bf1db4bb469b84c134569905a88
Jaro <jarobendominik@gmail.com>
2025-07-08 00:23:27 +0200

    added Rigidbody to fire and made fire bouncy wounzy douncy kabouncy

--- Disobey1.0/Assets/Scripts/WelcomeScene/Bottle.cs
+++ Disobey1.0/Assets/Scripts/WelcomeScene/Bottle.cs
@@ -7,8 +7,18 @@ public class Bottle : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Police")) { 
-            Instantiate(firePrefab, transform.position, Quaternion.identity);
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Police")) {
+
+            ContactPoint contact = collision.contacts[0];
+            Vector3 awayFromCollision = (contact.normal);
+
+            GameObject fire = Instantiate(firePrefab, transform.position + awayFromCollision*0.5f, Quaternion.identity);
+
+            Rigidbody rb = fire.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddForce(awayFromCollision * 1f, ForceMode.Impulse);
+            }
             Destroy(gameObject);
         }
     }
